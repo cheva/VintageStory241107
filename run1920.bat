1405f95d303f00185600dc8858384f22657a2d1c
Dee <dee.covan@gmail.com>
2024-11-03 20:01:28 +0200

    Nice Lakke Started

---
+++ run1920.bat
@@ -0,0 +1,4 @@
+C:\Programs\QRes.exe /x:1920 /y:1080
+copy clientsettings.json.fix.1920x1080 clientsettings.json
+"C:\Games\VintageStory-1.20.0-pre.6\PlayVintageStory.exe"
+copy clientsettings.json clientsettings.json.fix.1920x1080
